dcd1801df2eeac5aac80c1f5c6688b57faaada94
Ruslan <ruslan.mirets@gmail.com>
2022-06-29 18:16:03 +0300

    Init CLIENT

---
+++ client/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../app/assets/styles/globals.scss';
+import type { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
+
+export default App;
